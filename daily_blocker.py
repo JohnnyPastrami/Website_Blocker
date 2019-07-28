9e8b9d85abde3a5773d324fc3eb3eed2bec460f4
johnnypastrami <ryanleemcdonough@gmail.com>
2019-07-28 10:54:06 -0700

    reading and changing host file now

--- daily_blocker.py
+++ daily_blocker.py
@@ -8,6 +8,13 @@ website_list=["www.facebook.com","facebook.com","yahoo.com"]
 while True:
     if  dt(dt.now().year, dt.now().month,dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month,dt.now().day, 16):
         print("working hours!")
+        with open(hosts_path,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+website+"\n")
     else:
         print("fun hours :)")
     time.sleep(5)
