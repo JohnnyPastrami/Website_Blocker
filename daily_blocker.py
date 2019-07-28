cde7f017936c55bfad7d48e1acbf554ba0d14c52
johnnypastrami <ryanleemcdonough@gmail.com>
2019-07-28 11:27:00 -0700

    program now removes website blocking outsite of hours

--- daily_blocker.py
+++ daily_blocker.py
@@ -7,7 +7,7 @@ website_list=["www.facebook.com","facebook.com","yahoo.com"]
 
 
 while True:
-    if  dt(dt.now().year, dt.now().month,dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month,dt.now().day, 16):
+    if  dt(dt.now().year, dt.now().month,dt.now().day, 15) < dt.now() < dt(dt.now().year, dt.now().month,dt.now().day, 16):
         print("working hours!")
         with open(hosts_path,'r+') as file:
             content=file.read()
@@ -17,5 +17,11 @@ while True:
                 else:
                     file.write(redirect+" "+website+"\n")
     else:
-        print("fun hours :)")
+        with open(hosts_path, 'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
     time.sleep(5)
